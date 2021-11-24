e95bb9191d701e61a611d045e7114b87f68a576c
icalim <94712138+icalim@users.noreply.github.com>
2021-11-25 05:42:29 +0900

    Update auto_trade_upbit.py

--- auto_trade_upbit.py
+++ auto_trade_upbit.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "your-access"
-secret = "your-secret"
+access = "vofCKQNu8mT9Px7dxexU5UGu5Oevnbaj7NrBU2yG"
+secret = "DkSeBv9hvH6QkfmndcdWUz1xZf6O9Pxtudu0H0yx"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -44,7 +44,7 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
+            target_price = get_target_price("KRW-BTC", 0.7)
             current_price = get_current_price("KRW-BTC")
             if target_price < current_price:
                 krw = get_balance("KRW")
